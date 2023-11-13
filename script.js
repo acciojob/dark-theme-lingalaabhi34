32b7a5e7e588a312fa8533f588823793a7744ab3
lingalaabhi34 <122089325+lingalaabhi34@users.noreply.github.com>
2023-11-13 13:25:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
 
+// Get references to the elements
+const appDiv = document.getElementById('app');
+const swapButton = document.getElementById('swap');
+
+// Define the swapTheme function
+function swapTheme() {
+  // Toggle 'night' class on the #app div
+  appDiv.classList.toggle('night');
+
+  // Toggle 'button_night' class on the #swap button
+  swapButton.classList.toggle('button_night');
+}
+
+// Attach the swapTheme function to the button's click event
+swapButton.addEventListener('click', swapTheme);
